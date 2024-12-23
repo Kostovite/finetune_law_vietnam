b2f3d3d0f5ca6733fb630540ecbec4d5e76e2fe5
mtnkhang27 <119095083+mtnkhang27@users.noreply.github.com>
2024-12-23 17:12:28 +0700

    update: keep login when refresh page

--- app/src/routers/PrivateRouter/index.jsx
+++ app/src/routers/PrivateRouter/index.jsx
@@ -1,11 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase/firebase";
+import { auth } from "../../firebase/firebase"; // Import Firebase authentication
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
+  // If still loading, you can show a loader or wait for authentication state
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // If user is authenticated, allow access to children (e.g., the /chat page)
   return user ? children : <Navigate to="/login" />;
 };
 
